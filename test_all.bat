7afcc256660bb79f840b5a72381ccf175c65a628
Vincent <vichenzo-thebaud@hotmail.com>
2024-03-03 21:33:45 +0100

    Extensions individual tests
    mock-socket for ws only + removed mocha-webdriver
    
    Remove root libs + htmx as dev dependency
    
    Fix paths + all tests pass now
    
    Prevent annoying echos + exit with error on first fail
    
    Bat script to run all extensions tests from root
    
    To each extension its own test folder

---
+++ test_all.bat
@@ -0,0 +1,9 @@
+@echo off
+for /d %%G in (src/*) do @(
+    if exist "src\%%~nxG\test" (
+        (cd "src\%%~nxG" && npm i -D && npm t) || (@echo ERRORS in %%~nxG && exit /b !ERRORLEVEL!)
+        cd ..\..
+    ) else (
+        @echo %%~nxG doesn't have any test, skipping
+    )
+)
