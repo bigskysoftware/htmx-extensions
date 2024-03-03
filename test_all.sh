f44f08b44c11d9615d7fd57bb228026a3f6bdd0b
Vincent <vichenzo-thebaud@hotmail.com>
2024-03-03 22:48:47 +0100

    Bash script equivalent to run all tests

---
+++ test_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for dir in src/*; do
+    if [ -d "$dir/test" ]; then
+        (
+            cd "$dir"
+            npm i -D && npm t || { echo "ERRORS in $(basename "$dir")"; exit $?; }
+            cd ../../..
+        )
+    else
+        echo "$(basename "$dir") doesn't have any test, skipping"
+    fi
+done
