2602e857a64d06a3ab6bbeb7110ea3082879c474
Carson Gross <carson@bigsky.software>
2024-03-13 16:34:09 -0600

    more prep work for 2.0 extensions

---
+++ scripts/npm-update-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for dir in src/*; do
+    if [ -f "$dir/package.json" ]; then
+        (
+            cd "$dir"
+            npm i -D
+            cd ../../..
+        )
+    else
+        echo "$(basename "$dir") doesn't have a package.json, skipping"
+    fi
+done
