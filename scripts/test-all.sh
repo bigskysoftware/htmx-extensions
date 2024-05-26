629803afbf968357fe7ee72ab5a1c69a1fbfa4b9
Vincent <vichenzo-thebaud@hotmail.com>
2024-05-26 12:51:30 +0200

    Fix test-all.sh : properly exit on any extension error

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -6,9 +6,14 @@ for dir in $SCRIPT_DIR/../src/*; do
     if [ -d "$dir/test" ]; then
         (
             cd "$dir"
-            npm i -D && npm t || { echo "ERRORS in $(basename "$dir")"; exit $?; }
-            cd ../..
+            npm i -D && npm t
         )
+        # Check the exit status of the subshell
+        if [ $? -ne 0 ]; then
+           echo "ERRORS in $(basename "$dir")"
+           exit 1
+        fi
+        cd ../..
     else
         echo "$(basename "$dir") doesn't have any test, skipping"
     fi
