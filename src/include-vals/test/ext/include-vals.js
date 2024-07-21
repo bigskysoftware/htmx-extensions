ea99b31e67dee39ca943fc4617f451e22157c418
Vincent <vichenzo-thebaud@hotmail.com>
2024-07-21 10:11:17 +0200

    Add currently failing test as per bigskysoftware/htmx#2616
    https://github.com/bigskysoftware/htmx/issues/2616

--- src/include-vals/test/ext/include-vals.js
+++ src/include-vals/test/ext/include-vals.js
@@ -19,4 +19,23 @@ describe('include-vals extension', function() {
     this.server.respond()
     params.foo.should.equal('bar')
   })
+
+  it('Includes stringified object properly', function() {
+    var params = {}
+    this.server.respondWith('POST', '/test', function(xhr) {
+      params = getParameters(xhr)
+      xhr.respond(200, {}, 'clicked')
+    })
+
+    make(`<form hx-ext="include-vals" include-vals="foo: {'value1':'bar','value2':'test'}" hx-post="/test">
+            <input type="text" name="value" value="text">
+            <button id="btn">Click me</button>
+        </form>`)
+    var btn = byId("btn")
+    btn.click()
+    this.server.respond()
+
+    params.value.should.equal('text')
+    params.foo.should.equal('{"value1":"bar","value2":"test"}')
+  })
 })
