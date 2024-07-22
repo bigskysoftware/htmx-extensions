594a21a89080b09cbcc6d7ba4c83af30f1ef0c0e
Vincent <vichenzo-thebaud@hotmail.com>
2024-07-22 09:16:14 +0200

    Fix hx-vals/hx-vars encoding along json-enc (#57)
    * Fix hx-vals/hx-vars encoding along json-enc #2735
    
    * Collapse value assignment

--- src/json-enc/json-enc.js
+++ src/json-enc/json-enc.js
@@ -1,12 +1,28 @@
-htmx.defineExtension('json-enc', {
-  onEvent: function(name, evt) {
-    if (name === 'htmx:configRequest') {
-      evt.detail.headers['Content-Type'] = 'application/json'
-    }
-  },
+(function() {
+  let api
+  htmx.defineExtension('json-enc', {
+    init: function(apiRef) {
+      api = apiRef
+    },
 
-  encodeParameters: function(xhr, parameters, elt) {
-    xhr.overrideMimeType('text/json')
-    return (JSON.stringify(parameters))
-  }
-})
+    onEvent: function(name, evt) {
+      if (name === 'htmx:configRequest') {
+        evt.detail.headers['Content-Type'] = 'application/json'
+      }
+    },
+
+    encodeParameters: function(xhr, parameters, elt) {
+      xhr.overrideMimeType('text/json')
+
+      const vals = api.getExpressionVars(elt)
+      const object = {}
+      parameters.forEach(function(value, key) {
+        // FormData encodes values as strings, restore hx-vals/hx-vars with their initial types
+        object[key] = Object.hasOwn(vals, key) ? vals[key] : value
+      })
+
+      return (JSON.stringify(object))
+    }
+  })
+
+})()
