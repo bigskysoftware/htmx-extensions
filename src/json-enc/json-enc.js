ef7073c70679d6aebc29bee7ba2daf8b46620585
Vincent <vichenzo-thebaud@hotmail.com>
2025-01-05 09:24:06 +0100

    Simplify

--- src/json-enc/json-enc.js
+++ src/json-enc/json-enc.js
@@ -14,27 +14,23 @@
     encodeParameters: function(xhr, parameters, elt) {
       xhr.overrideMimeType('text/json')
 
-      // group FormData parameters by key
-      const groupedParameters = Array.from(parameters.entries()).reduce((grouped, [key, value]) => {
-        if (Object.hasOwn(grouped, key)) {
-          if (!Array.isArray(grouped[key])) {
-            grouped[key] = [grouped[key]]
+      const object = {}
+      parameters.forEach(function(value, key) {
+        if (Object.hasOwn(object, key)) {
+          if (!Array.isArray(object[key])) {
+            object[key] = [object[key]]
           }
-          grouped[key].push(value)
+          object[key].push(value)
         } else {
-          grouped[key] = value
+          object[key] = value
         }
-        return grouped;
-      }, {});
+      })
 
       const vals = api.getExpressionVars(elt)
-      const object = Object.fromEntries(
-          Object.entries(groupedParameters).map(([key, value]) => {
-            // FormData encodes values as strings, restore hx-vals/hx-vars with their initial types
-            const typedValue = Object.hasOwn(vals, key) ? vals[key] : value
-            return [key, typedValue]
-          })
-      )
+      Object.keys(object).forEach(function(key) {
+        // FormData encodes values as strings, restore hx-vals/hx-vars with their initial types
+        object[key] = Object.hasOwn(vals, key) ? vals[key] : object[key]
+      })
 
       return (JSON.stringify(object))
     }
