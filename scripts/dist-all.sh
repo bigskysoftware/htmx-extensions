cc5038aa67092afeefe38ce984715b1380b6af63
Mariss Tubelis <mariss@mariss.no>
2025-02-12 09:35:02 +0100

    Provide ESM and minified versions (#123)
    * Add esm, cjs, amd, min and gzip support
    
    * Update package-lock.json files
    
    * Add script tag print with version number and SRI hash in dist script
    
    * Fix error in script tag print in dist script
    
    * Add script comment, remove amd and cjs support
    
    * Update dist-all.sh to be executable
    
    * Run dist script in the beginning of who-needs-a-release.rb

---
+++ scripts/dist-all.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# This script must be run from `npm run dist` in order to access `node` and `uglifyjs` commands
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Traverse all directories which contain a `src` folder
+for dir in $SCRIPT_DIR/../src/*; do
+    EXTENSION_NAME=$(basename "$dir")
+    if [ -e "$dir/$EXTENSION_NAME.js" ]; then
+        (
+            cd "$dir"
+            EXTENSION_SRC="$dir/$EXTENSION_NAME.js"
+
+            # Prepare a clean dist directory
+            mkdir -p dist
+            rm -rf dist/*.js  dist/*.ts  dist/*.gz
+
+            # Regular IIFE script
+            cp $EXTENSION_SRC dist/$EXTENSION_NAME.js
+
+            # Generate minified script
+            uglifyjs -m eval -o dist/$EXTENSION_NAME.min.js dist/$EXTENSION_NAME.js
+
+            # Generate gzipped script
+            gzip -9 -k -f dist/$EXTENSION_NAME.min.js > dist/$EXTENSION_NAME.min.js.gz
+
+            # Generate ESM script
+            cat > dist/$EXTENSION_NAME.esm.js << EOF
+import htmx from 'htmx.org';
+$(cat $EXTENSION_SRC)
+EOF
+
+            # Print success message and updated script tag with SRI hash
+            echo
+            echo "$EXTENSION_NAME distribution files generated"
+            SRI_HASH=`openssl dgst -sha384 -binary $dir/dist/$EXTENSION_NAME.min.js | openssl base64 -A`
+            VERSION=`node -p -e "require('$dir/package.json').version"`
+            echo "<script src=\"https://unpkg.com/htmx-ext-$EXTENSION_NAME@$VERSION\" integrity=\"sha384-$SRI_HASH\" crossorigin=\"anonymous\"></script>"
+        )
+        cd ../..
+    else
+        echo
+        echo "Extension source file "$EXTENSION_NAME.js" doesn't exist"
+    fi
+done
